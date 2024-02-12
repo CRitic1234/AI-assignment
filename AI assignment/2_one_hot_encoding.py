f473d45cd517f27b22a853ecf59580085df39f58
CRitic1234 <148579262+CRitic1234@users.noreply.github.com>
2024-02-12 10:28:24 +0530

    one_hot_encoding.py

--- AI assignment/2_one_hot_encoding.py
+++ AI assignment/2_one_hot_encoding.py
@@ -1,7 +1,7 @@
 import numpy as np
 arr = np.array([0,5,7,1,9])
-encoded_arr = np.zeros((arr.size, arr.max()+1), dtype=int)
-encoded_arr[np.arange(arr.size),arr] = 1
+encoded_arr = np.zeros((arr.size, arr.max()+1), dtype=int)  #array of all values equal to zero of size range(0,10)
+encoded_arr[np.arange(arr.size),arr] = 1    #assigning values of rows and column to 1 acc to values in arr 
 print(encoded_arr)
 
 
